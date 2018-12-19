fb34cf7ca7ce7cc47b5137396660f59c0a31fcb2
Jittapan Pluemsumran <secret@jittapan.app>
2018-12-19 20:26:39 +0700

    Add support for web.facebook.com and mobile.facebook.com

--- fbclid.js
+++ fbclid.js
@@ -1,5 +1,6 @@
 const param = 'fbclid';
-if (document.referrer === "https://www.facebook.com/" && location.search.indexOf(param + '=') !== -1) {
+const pattern = /https:\/\/(?:www|web|mobile).facebook.com\//g;
+if (document.referrer.match(pattern) !== null && location.search.indexOf(param + '=') !== -1) {
     const url = new URL(location);
     url.searchParams.delete(param);
     history.replaceState(null, '', url.href);
