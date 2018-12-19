f1adf2f6d3b3b1fb3d0d031c9d754ca505355dfb
Jittapan Pluemsumran <nightsheep@outlook.com>
2018-12-19 20:38:56 +0700

    Extended pattern to cover *.facebook.com domains

--- fbclid.js
+++ fbclid.js
@@ -1,5 +1,5 @@
 const param = 'fbclid';
-const pattern = /https:\/\/(?:www|web|mobile).facebook.com\//g;
+const pattern = /https:\/\/(?:[^/]+\.)?facebook.com\//g;
 if (document.referrer.match(pattern) !== null && location.search.indexOf(param + '=') !== -1) {
     const url = new URL(location);
     url.searchParams.delete(param);
