42e80143f7e0102693c891fd3b2e4e4dccca3320
Jakub Bouček <pan@jakubboucek.cz>
2018-12-19 14:56:58 +0100

    Refactoring: make variable name more specific
    As optimize #1

--- fbclid.js
+++ fbclid.js
@@ -1,6 +1,6 @@
 const param = 'fbclid';
-const pattern = /https:\/\/(?:[^/]+\.)?facebook.com\//g;
-if (document.referrer.match(pattern) !== null && location.search.indexOf(param + '=') !== -1) {
+const referrerPattern = /https:\/\/(?:[^/]+\.)?facebook.com\//g;
+if (document.referrer.match(referrerPattern) !== null && location.search.indexOf(param + '=') !== -1) {
     const url = new URL(location.href);
     url.searchParams.delete(param);
     history.replaceState(null, '', url.href);
