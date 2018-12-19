0a082d664fb2ad6b371fa168030b319b55499b93
Jakub Bouček <pan@jakubboucek.cz>
2018-12-19 14:47:36 +0100

    Fix strict type mismatch
    URL() requires string in parameter, see https://developer.mozilla.org/en-US/docs/Web/API/URL/URL#Parameters

--- fbclid.js
+++ fbclid.js
@@ -1,6 +1,6 @@
 const param = 'fbclid';
 if (document.referrer === "https://www.facebook.com/" && location.search.indexOf(param + '=') !== -1) {
-    const url = new URL(location);
+    const url = new URL(location.href);
     url.searchParams.delete(param);
     history.replaceState(null, '', url.href);
 }
