db3cef95a22298660176adadc2fa535b34670884
Jakub Bouček <pan@jakubboucek.cz>
2018-12-19 15:01:17 +0100

    Security: Prevent to match unwanted referrer
    Example unwanted matched URL: http://example.com/?url=https://facebook.com/
    
    As optimize #1

--- fbclid.js
+++ fbclid.js
@@ -1,5 +1,5 @@
 const param = 'fbclid';
-const referrerPattern = /https:\/\/(?:[^/]+\.)?facebook.com\//g;
+const referrerPattern = /^https:\/\/(?:[^/]+\.)?facebook.com\//g;
 if (document.referrer.match(referrerPattern) !== null && location.search.indexOf(param + '=') !== -1) {
     const url = new URL(location.href);
     url.searchParams.delete(param);
